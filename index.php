e809dc43b3673a321fb756108700f1c2a5ebf551
Lilitha27 <lilithakganya@gmail.com>
2019-08-29 16:51:09 +0200

    added links

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/style2.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>To do list</title>
@@ -50,6 +50,6 @@ function displaylist(){
     ?>
 
 
-<script src="js/main2.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
