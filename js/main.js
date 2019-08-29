c6061ad184fa8b902751d123cb3802a7e21f015a
Lilitha27 <lilithakganya@gmail.com>
2019-08-29 16:33:03 +0200

    added functionality

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var itemDone = 0;
+    $("li").each(function(c){
+        $(this).click(function(){
+        checked = c;
+         //check the status of li before setting text-decoration
+         if(itemDone == 0){
+            $(this).css("text-decoration", "line-through");
+            itemDone = 1;
+            sessionStorage.setItem(checked, itemDone);
+            console.log(itemDone);
+ 
+           }else{
+               $(this).css("text-decoration", "none");
+               itemDone = 0;
+               sessionStorage.setItem(checked, itemDone);
+               console.log(itemDone);
+            }
+        });
+    });
+});
