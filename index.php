f5f1ba5f95fc0d98ec7421d9d351bcad1a84e3be
Lilitha27 <lilithakganya@gmail.com>
2019-08-29 16:25:32 +0200

    added heading

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+// session_destroy();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style2.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>To do list</title>
+</head>
